2525cf3369738642a30ca4f239974140c6a71bdd
mavelleira11 <62119673+mavelleira11@users.noreply.github.com>
2020-03-12 17:22:19 -0400

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,9 @@
+# Mariel Avelleira
+# My Integration Project
+    #Guide to Python
+
+name = input()
+print("Hello,", name + ".\nWelcome to Python!")
+print("This is a guide on how to use Python for Math")
+
+
